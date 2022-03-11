aee173e89fb9fd1dd201c7215f5c95a1767914b6
bragin45 <tech45ru@gmail.com>
2022-03-11 23:05:35 +0500

    Added Repository

--- src/main/java/ru/netology/repository/Repository.java
+++ src/main/java/ru/netology/repository/Repository.java
@@ -1,2 +1,33 @@
-package ru.netology.repository;public class Repository {
+package ru.netology.repository;
+
+import ru.netology.product.Product;
+
+public class Repository {
+    private Product[] items = new Product[0];
+
+    public void save(Product product){
+        int length = items.length + 1;
+        Product[] tmp = new Product[length];
+        System.arraycopy(items,0,tmp,0,items.length);
+        int lastIndex = tmp.length - 1;
+        tmp[lastIndex] = product;
+        items = tmp;
+    }
+
+    public Product[] findAll(){
+        return items;
+    }
+
+    public void removeById(int id){
+        int length = items.length - 1;
+        Product[] tmp = new Product[length];
+        int index = 0;
+        for (Product item:items) {
+            if (item.getId() !=id){
+                tmp[index] = item;
+                index++;
+            }
+        }
+        items = tmp;
+    }
 }
